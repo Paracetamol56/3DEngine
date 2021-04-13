51f17fe700af3d6ebb6b3398d337b5b37b059518
Mathéo <matheo.galu56@gmail.com>
2021-04-13 19:19:02 +0200

    Add default cube mesh for testing

--- 3DWireframeEngine/3DWireframeEngine/Main.cpp
+++ 3DWireframeEngine/3DWireframeEngine/Main.cpp
@@ -3,6 +3,27 @@
 // Constructor
 CMain::CMain() : wxFrame(nullptr, wxID_ANY, "WX Window template")
 {
+	// ========= Example default cube mesh ========= //
+	// Bottom face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// Front face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// Right face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// Back face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// Left face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// Top face
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	m_mesh.addTriangle(CTriangle(CVector3D(0.0f, 0.0f, 0.0f), CVector3D(1.0f, 0.0f, 0.0f), CVector3D(1.0f, 1.0f, 0.0f)));
+	// ======== END Example default cube mesh =======//
+
 }
 
 // Destructor
--- 3DWireframeEngine/3DWireframeEngine/Main.h
+++ 3DWireframeEngine/3DWireframeEngine/Main.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <wx/wx.h>
+#include "Triangle.h"
+#include "Vector3D.h"
+#include "Mesh.h"
 
 class CMain : public wxFrame
 {
+private:
+	CMesh m_mesh;
+
 public:
 	// Public constructor and destructor
 	CMain();
