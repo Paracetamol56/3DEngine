eef1f1a6a83ed7c5be12e776b598451011c3e3e3
Mathéo <matheo.galu56@gmail.com>
2021-05-30 18:48:40 +0200

    Add memory leaks detection

---
+++ 3DEngine/3DEngine/wxmemdbg.h
@@ -0,0 +1,28 @@
+/*
+ * Created on Tue May 30 2021
+ *
+ * Copyright (c) 2021 - Mathéo Galuba - All Right Reserved
+ *
+ * Licensed under the Apache License, Version 2.0
+ * Available on GitHub at https://github.com/Paracetamol56/3DWireframeEngine
+ */
+
+#pragma once
+
+// This file detects memory leaks
+
+#ifndef WXMEMDBG_H_INCLUDED
+#define WXMEMDBG_H_INCLUDED
+
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK ,__FILE__, __LINE__)
+#else
+#define DEBUG_NEW new
+#endif
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif 
+
+#endif // WXMEMDBG_H_INCLUDED
